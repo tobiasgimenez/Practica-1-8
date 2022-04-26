cf7375646ca238f1c9b6b176a473d55f60c4c381
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-26 16:31:00 -0300

    cree estructuraif.js y resolvi el problema 5

---
+++ estructuraif.js
@@ -0,0 +1,11 @@
+var numeroUsuario = prompt("Ingrese un numero cualquiera", "0");
+
+if( numeroUsuario == 3 ){
+    document.write (`
+        <h3> Si es igual </h3>
+`);
+}else{
+    document.write (`
+    <h3> NO es igual </h3>    
+    `);
+}
